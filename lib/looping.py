cfb58341dfb433e5c4888f8773fefe25acb4be6a
Vaschell Mkenzie <mvaschell@gmail.com>
2023-07-14 08:27:25 -0400

    almost finished

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,18 @@
 #!/usr/bin/env python3
 
 def happy_new_year():
-    # code goes here!
-    pass
+    counter = 10
+    while counter > 0:
+        print(counter)
+        counter-=1
+    print("Happy New Year!")   
+        
 
 def square_integers(int_list):
-    # code goes here!
-    pass
-
+    int_list_squared = [number**2 for number in int_list]
+    return int_list_squared
+ 
+    
 def fizzbuzz():
-    # code goes here!
-    pass
+    i=1
+    for i <= 100:
