e0c6699aa53fa7de5aca507c5c9e6035170fc0ce
Vaschell Mkenzie <mvaschell@gmail.com>
2023-07-15 12:38:47 -0400

    finished

--- lib/looping.py
+++ lib/looping.py
@@ -14,5 +14,12 @@ def square_integers(int_list):
  
     
 def fizzbuzz():
-    i=1
-    for i <= 100:
+   for i in range(1,101):
+    if (i % 3 == 0 and i % 5 == 0):
+       print("FizzBuzz")
+    elif i % 3 == 0:
+       print("Fizz") 
+    elif i % 5 == 0:
+       print("Buzz")
+    else:
+       print(i)
